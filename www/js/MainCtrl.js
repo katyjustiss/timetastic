2441176487cc9dad4dd2b04d376e870f6f4c33e2
Katy Justiss <katy.justiss@gmail.com>
2015-07-09 15:19:34 -0500

    added comments to js

--- www/js/MainCtrl.js
+++ www/js/MainCtrl.js
@@ -1,51 +1,50 @@
 angular.module('timer')
   .controller('MainCtrl', ['$scope', '$timeout', function($scope, $timeout) {
 
-    $scope.counter = '0';
-
+    $scope.counter = 0;
     var userTimeout = null; // the current timeoutID
-    // actual timer method, counts down every second, stops on zero
+
+    // Timer FUNCTION: counts down every second, stops on zero
     $scope.onTimeout = function() {
-        if($scope.counter ===  0) {
+        if($scope.counter ===  0) { //if 0, end timer and broadcast info
             $scope.$broadcast('timer-stopped', 0);
             $timeout.cancel(userTimeout);
             return;
         }
-        $scope.counter--;
-        userTimeout = $timeout($scope.onTimeout, 1000);
+        $scope.counter--; //decrement
+        userTimeout = $timeout($scope.onTimeout, 1000); //recursively call until ended above at 0.
     };
+    // START TIMER
     $scope.startTimer = function() {
         userTimeout = $timeout($scope.onTimeout, 1);
     };
-
+    //RESET TIMER
     $scope.resetTimer = function () {
-      $scope.counter = '0';
+      $scope.counter = 0;
       $timeout.cancel(userTimeout);
     };
-    // stops and resets the current timer
+    // STOP and RESET the current timer
     $scope.stopTimer = function() {
         $scope.$broadcast('timer-stopped', $scope.counter);
-        // $scope.counter = 30;
         $timeout.cancel(userTimeout);
     };
-    // triggered, when the timer stops, you can do something here, maybe show a visual indicator or vibrate the device
+    // Triggered when timer stops. Perform actions when counter reaches zero.
     $scope.$on('timer-stopped', function(event, remaining) {
         if(remaining === 0) {
           console.log('your time ran out!');
           navigator.vibrate(3000);
-          navigator.notification.beep(5);
-          navigator.notification.alert(
-            'RED ALERT');
+          navigator.notification.beep(1);
+          navigator.notification.alert('RED ALERT');
         }
     });
-    //grabbing numbers
+    //grabbing keypad numbers
     $scope.number = function(num){
       num = num.toString();
-      if($scope.counter === '0'){
-        $scope.counter = num;
+      if($scope.counter === 0){
+        $scope.counter = num; //replacing counter with single number
       }else{
-        $scope.counter += num;
+        $scope.counter += num; //add string numbers together and then replacing counter
       }
     }
 
-}]);
+}]);//end of controller
