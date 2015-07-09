4eef6a698bb6e1ca66b181329da311fce68024b2
Brien Pafford <bcpaff@gmail.com>
2015-07-09 11:09:00 -0500

    Start stop button, and timer. console logs when timer is done

--- www/js/MainCtrl.js
+++ www/js/MainCtrl.js
@@ -1,5 +1,30 @@
-angular
-  .module('timer')
-  .controller('MainCtrl', ['$scope', function($scope) {
-
-  }])
+angular.module('timer')
+  .controller('MainCtrl', ['$scope', '$timeout', function($scope, $timeout) {
+    $scope.counter = 5;
+    var userTimeout = null; // the current timeoutID
+    // actual timer method, counts down every second, stops on zero
+    $scope.onTimeout = function() {
+        if($scope.counter ===  0) {
+            $scope.$broadcast('timer-stopped', 0);
+            $timeout.cancel(userTimeout);
+            return;
+        }
+        $scope.counter--;
+        userTimeout = $timeout($scope.onTimeout, 1000);
+    };
+    $scope.startTimer = function() {
+        userTimeout = $timeout($scope.onTimeout, 1000);
+    };
+    // stops and resets the current timer
+    $scope.stopTimer = function() {
+        $scope.$broadcast('timer-stopped', $scope.counter);
+        $scope.counter = 30;
+        $timeout.cancel(userTimeout);
+    };
+    // triggered, when the timer stops, you can do something here, maybe show a visual indicator or vibrate the device
+    $scope.$on('timer-stopped', function(event, remaining) {
+        if(remaining === 0) {
+            console.log('your time ran out!');
+        }
+    });
+}]);
